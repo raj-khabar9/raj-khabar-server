fe4349e27b803100ed5e486e0d38d3d9216ce54a
irishi <rishi@cyntexa.com>
2025-06-29 17:18:59 +0530

    updated post routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -34,7 +34,7 @@ postRouter.get("/get-caroucel-posts", getCaroucelPost);
 postRouter.get("/posts/:slug", getPostBySlug); // Assuming you want to use the same controller for getting post by slug
 postRouter.get("/posts/id/:id", getPostById); // Assuming you want to use the same controller for getting post by ID
 postRouter.delete("/:id", authMiddleware, deletePost);
-postRouter.patch("/:id", upload.single("image"), authMiddleware, updatePost);
+postRouter.put("/:id", upload.single("image"), authMiddleware, updatePost);
 postRouter.get("/related/:slug", getRelatedPosts);
 
 export { postRouter };
